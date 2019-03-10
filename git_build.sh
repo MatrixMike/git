902f1ddf7d7ed9ab636eda2045fe8cd07dbb2599
Mike J H <matrixmike@icloud.com>
2019-03-10 12:39:51 +1100

    build shell scripts

---
+++ git_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+make configure ;# as yourself
+./configure --prefix=/usr ;# as yourself
+make all doc ;# as yourself
+# make install install-doc install-html;# as root
+
---
+++ git_build2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo make install install-doc install-html;# as root
