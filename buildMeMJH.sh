64c300fdb13dd1e8c71456ee389ee9c0f66c2559
Mike LM18 optiplex <matroxmike@icloud.com>
2019-03-14 14:24:47 +1100

    practical build sequence 2

--- buildMeMJH.sh
+++ buildMeMJH.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+sudo apt install asciidoc  ; install for documentation
+make configure ;# as yourself
+./configure --prefix=/usr ;# as yourself
 make prefix=/usr all doc info
+make all doc ;# as yourself
+make install install-doc install-html;# as root
+
+
